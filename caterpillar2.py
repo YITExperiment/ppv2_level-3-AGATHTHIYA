cffd4bfde46e3f9d8384eed0acdb9530f3f4bf28
AGATHTHIYA <134824803+AGATHTHIYA@users.noreply.github.com>
2023-06-28 16:20:22 +0530

    Add files via upload

--- caterpillar2.py
+++ caterpillar2.py
@@ -1,6 +1,5 @@
+import random
 import turtle as t
-import random as rd
-
 t.bgcolor('yellow')
 
 caterpillar = t.Turtle()
@@ -11,22 +10,38 @@ caterpillar.penup()
 caterpillar.hideturtle()
 
 leaf = t.Turtle()
-leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
-t.register_shape('leaf',leaf_shape)
+leaf_shape = ((0,0),(14,2),(18,6),(20,20), \
+(6,18),(2,14))
+t.register_shape('leaf', leaf_shape)
 leaf.shape('leaf')
 leaf.color('green')
 leaf.penup()
 leaf.hideturtle()
-leaf.speed('slow')
+leaf.speed(0)
 
-game_starterd =False
-text_turtle = t.Turtle()
-text_turtle.write('Press SPACE to start',align='center',fond=('Arial',16,'bold'))
-text_turtle.hideturtle()
+def outside_window():
+    pass
+def game_over():
+    pass
+def display_score(current_score):
+    pass
+def place_leaf():
+    pass
 
-score_turtle = t.Turtle()
-score_turtle.hideturtle()
-score_turtle.speed(0)
+def start_game():
+    global game_started
+    if game_started:
+        return
+    game_started = Ture
+
+    score = 0
+    text_turtle.clear()
+
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar
+    
+  
 
 def outside_window():
   left_wall = -t.window_width()/2
